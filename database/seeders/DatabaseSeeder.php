26cc85ad665db7178c226e84ac5f8bfc98d8ebe6
Sourabh <teambcodecraft@gmail.com>
2024-12-05 12:56:47 +0530

    new change issue fix in membership

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Call other seeders
+        $this->call(PaymentSettingSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(ManageUsersTableSeeder::class);
@@ -21,7 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionsSeeder::class);
         $this->call(RoleHasPermissionsSeeder::class);
         $this->call(SmtpConfigurationSeeder::class);
-        $this->call(PaymentSettingSeeder::class);
+
         // Call PluginSeeder
         $this->call(PluginSeeder::class);
         $this->call(MembershipPlanSeeder::class);
